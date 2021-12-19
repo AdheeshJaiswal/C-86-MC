bf124644c8c9edde599f3a1e78ed985c1d8d5001
AdheeshJaiswal <91142677+AdheeshJaiswal@users.noreply.github.com>
2021-12-19 19:45:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+const { fabric } = require("./fabric");
+
+
+varcanvas = new fabric.Canvas('myCanvas');
+
+player_x = 10;
+player_y = 10;
+
+block_image_width = 30;
+block_image_height = 30;
+
+var player_object = "";
+var block_image_object= "";
+
+function player_update()
+{
+    fabric.Image.fromURL("player.png", function(Img)
+    
+   {player_object = Img;
+
+    player_object.scaleaToWidth(150);
+    player_object.scaleaToHeight(140);
+    player_object.set({
+        top:player_y,
+        left:player_x
+    });
+canvas.add(player_object);
+   }) 
+
+    
+}
+
+function new_image(get_image)
+{
+	fabric.Image.fromURL(get_image, function(Img) {
+	block_image_object = Img;
+
+	block_image_object.scaleToWidth(block_image_width);
+	block_image_object.scaleToHeight(block_image_height);
+	block_image_object.set({
+	top:player_y,
+	left:player_x
+	});
+	canvas.add(block_image_object);
+
+	});
+
+}
